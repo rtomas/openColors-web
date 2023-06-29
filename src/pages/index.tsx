25f19953357abb477cf03f3c8137a0b5359f05b6
Gera Barboni <63870012+geraBarboni@users.noreply.github.com>
2023-06-29 17:56:49 -0300

    menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,25 @@ import ColorRow from "@/components/colorRow";
 import { useOpenColorsContract } from "@/hooks/useOpenColorsContract";
 
 import metadata from "@/contract/open_colors.json";
+import Menu from "@/components/Menu";
 
 export default function Home() {
-    const { colorList, loading } = useOpenColorsContract();
+  const { colorList, loading } = useOpenColorsContract();
 
-    /*     useEffect(() => {
+  /*     useEffect(() => {
         setListColors([rgbToHex(255, 255, 255), rgbToHex(0, 0, 0)]);
     }, []); */
 
-    useEffect(() => {
-        //console.log(colorList);
-    }, [colorList]);
+  useEffect(() => {
+    //console.log(colorList);
+  }, [colorList]);
 
-    return (
-        <main className="w-ful font-mono text-2xl">
-            {colorList.map((color, index, arr) => (
-                <ColorRow key={index} color1={color} color2={arr[index + 1]} />
-            ))}
-        </main>
-    );
+  return (
+    <main className="w-full h-screen py-4 font-mono text-2xl bg-gradient-to-br from-orange-400 to-blue-500">
+      <Menu />
+      {colorList.map((color, index, arr) => (
+        <ColorRow key={index} color1={color} color2={arr[index + 1]} />
+      ))}
+    </main>
+  );
 }

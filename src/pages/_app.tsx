8eefb4db08bd374df1a9cfb995b8c1a08e354204
Gera Barboni <63870012+geraBarboni@users.noreply.github.com>
2023-06-30 10:01:57 -0300

    add menu / fix UI / add color tx (#3)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,24 +6,25 @@ import { InkConfig } from "useink";
 
 import { RococoContractsTestnet } from "useink/chains";
 
-const UseInkProvider: React.ComponentType<React.PropsWithChildren<InkConfig>> = dynamic(() => import("useink").then(({ UseInkProvider }) => UseInkProvider), {
+const UseInkProvider: React.ComponentType<React.PropsWithChildren<InkConfig>> =
+  dynamic(() => import("useink").then(({ UseInkProvider }) => UseInkProvider), {
     ssr: false,
-});
+  });
 
 function App({ Component, pageProps }: AppProps) {
-    return (
-        <UseInkProvider
-            config={{
-                dappName: "openColors",
-                chains: [RococoContractsTestnet],
-                caller: {
-                    default: "5Dsykc2KUHcziwcTgZkHxyDDTotBJbGNh3BakfZ5PdDGMzfm",
-                },
-            }}
-        >
-            <Component {...pageProps} />
-        </UseInkProvider>
-    );
+  return (
+    <UseInkProvider
+      config={{
+        dappName: "openColors",
+        chains: [RococoContractsTestnet],
+        caller: {
+          default: "5Dsykc2KUHcziwcTgZkHxyDDTotBJbGNh3BakfZ5PdDGMzfm",
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </UseInkProvider>
+  );
 }
 
 export default App;
